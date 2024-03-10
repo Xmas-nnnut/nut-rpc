d1149d9f05b80e474191ba2c1f6a9d328d44c939
xuqingjian <xuqingjian894@gmail.com>
2024-03-10 15:14:34 +0800

    feat: 本地服务注册器

--- example-provider/src/main/java/com/xqj/example/provider/EasyProviderExample.java
+++ example-provider/src/main/java/com/xqj/example/provider/EasyProviderExample.java
@@ -14,7 +14,8 @@ import com.xqj.nutrpc.core.server.HttpServer;
 public class EasyProviderExample {
 
     public static void main(String[] args) {
-
+        // 注册服务
+        LocalRegistry.register(UserService.class.getName(), UserServiceImpl.class);
         // 启动 web 服务
         HttpServer httpServer = new VertxHttpServer();
         httpServer.doStart(8080);
